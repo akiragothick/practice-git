c38d0351db5951b86dadecac9d072b1f822bbc17
Ernesto Vargas <titto_44@hotmail.com>
2021-11-05 00:55:35 -0500

    mejora del merge

--- PracticeGit/Program.cs
+++ PracticeGit/Program.cs
@@ -18,17 +18,15 @@ namespace PracticeGit
 
 			//Console.WriteLine("Akra");
 
-<<<<<<< HEAD
 			Console.WriteLine("Akra fe"); //holiiii
 
 			string Holii2() => "";
 
 			Holii2();
-=======
+
 			Console.WriteLine("Akra feature6");
 
 			//cambio moises
->>>>>>> 7136a19ea6fddfb4dbe6790e6f957a26081b5923
 		}
 	}
 }
